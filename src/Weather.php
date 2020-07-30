1c1a8422e184a86f40e9a6abfd2d14952eced4ed
cc <chengguangxin927@gmail.com>
2020-07-30 11:27:11 +0800

    fix

--- src/Weather.php
+++ src/Weather.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * This file is part of the chenggx/weather.
- *
- * (c) chenggx
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
+
 
 namespace Chenggx\Weather;
 

18e1d46af9cd06baeaced9558787e1d8238d6611
chenggx <chengguangxin927@gmail.com>
2020-07-30 03:27:23 +0000

    Apply fixes from StyleCI

--- src/Weather.php
+++ src/Weather.php
@@ -1,6 +1,13 @@
 <?php
 
-
+/*
+ * This file is part of the chenggx/weather.
+ *
+ * (c) chenggx
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Chenggx\Weather;
 
